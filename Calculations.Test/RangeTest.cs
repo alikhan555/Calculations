06f608b76b1fb8b1f251ef4b6bf7504ba4059387
Muhammad Ali <m.alikhan555@gmail.com>
2024-04-18 20:39:02 +0500

    Asserting a range of values

---
+++ Calculations.Test/RangeTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Calculations.Test
+{
+    public class RangeTest
+    {
+        [Fact]
+        public void Range_MustBetween8To50()
+        {
+            Assert.InRange(Ranges.Age, 8, 50);
+        }
+
+        [Fact]
+        public void Range_ProbablityMustBeGreaterThan80Percent()
+        {
+            Assert.NotInRange(Ranges.Probablity, 0, 0.800);
+        }
+
+        [Fact]
+        public void Range_PhoneNoMustBeIn10To15Digits()
+        {
+            Assert.InRange(Ranges.PhoneNo.Length, 10, 15);
+        }
+    }
+}
---
+++ Calculations/Ranges.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculations
+{
+    public class Ranges
+    {
+        public static int Age = 50;
+        public static double Probablity = 0.805;
+        public static string PhoneNo = "03051234567";
+    }
+}
