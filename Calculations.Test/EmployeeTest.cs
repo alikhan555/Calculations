900323dc1284c36d68411bf0c791dfde1c7e6a83
Muhammad Ali <m.alikhan555@gmail.com>
2024-04-18 21:35:03 +0500

    Asserting object types

---
+++ Calculations.Test/EmployeeTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Calculations.Test
+{
+    public class EmployeeTest
+    {
+        [Fact]
+        public void Employee_MustBePartTimeEmployee()
+        {
+            EmployeeFactory factory = new EmployeeFactory();
+            PartTimeEmployee partTimeEmployee = Assert.IsType<PartTimeEmployee>(factory.GetEmployee("PartTime"));
+            Assert.Equal(partTimeEmployee.HourlySalary, 101);
+        }
+
+        [Fact]
+        public void Employee_MustBeFullTimeEmployee()
+        {
+            EmployeeFactory factory = new EmployeeFactory();
+            FullTimeEmployee fullTimeEmployee = Assert.IsType<FullTimeEmployee>(factory.GetEmployee("FullTime"));
+            Assert.Equal(fullTimeEmployee.MonthlySalary, 24001);
+        }
+    }
+}
---
+++ Calculations/EmployeeFactory.cs
@@ -0,0 +1,35 @@
+﻿namespace Calculations
+{
+    public class EmployeeFactory
+    {
+        public Employee GetEmployee(string employeeType)
+        {
+            if (employeeType == "PartTime")
+            {
+                return new PartTimeEmployee() { HourlySalary = 101};
+            }
+            if (employeeType == "FullTime")
+            {
+                return new FullTimeEmployee() { MonthlySalary = 24001 };
+            }
+
+            return null;
+        }
+    }
+
+    public class Employee
+    {
+
+    }
+
+
+    public class PartTimeEmployee : Employee
+    {
+        public int HourlySalary { get; set; }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        public int MonthlySalary { get; set; }
+    }
+}
