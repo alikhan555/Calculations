af3cb06e97e9a8b9197c90031efc20c745bc04ca
Muhammad Ali <m.alikhan555@gmail.com>
2024-04-18 20:59:48 +0500

    Asserting Exceptions

---
+++ Calculations.Test/ExceptionTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Calculations.Test
+{
+    public class ExceptionTest
+    {
+        [Fact]
+        public void Exception_ThrowArgumentNullException_Hello()
+        {
+            MyExceptions myExceptions = new MyExceptions();
+            var exceptionResult = Assert.Throws<ArgumentException>(() => myExceptions.ThrowArgumentNullException(null));
+            Assert.Equal("hello", exceptionResult.Message);
+        }
+    }
+}
