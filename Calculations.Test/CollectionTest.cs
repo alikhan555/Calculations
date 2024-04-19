17ff72e20ed8bb840d26b90ce640a65aec354411
Muhammad Ali <m.alikhan555@gmail.com>
2024-04-19 21:28:06 +0500

    Collections and Traits

--- Calculations.Test/CollectionTest.cs
+++ Calculations.Test/CollectionTest.cs
@@ -5,24 +5,28 @@ namespace Calculations.Test
     public class CollectionTest
     {
         [Fact]
+        [Trait("Categories", "Fibo")]
         public void Fibonacci_NotHaveZero()
         {
             Assert.All(Collections.Fibonacci, x => Assert.NotEqual(0, x));
         }
 
         [Fact]
+        [Trait("Categories", "Fibo")]
         public void Fibonacci_MustHave5()
         {
             Assert.Contains(5, Collections.Fibonacci);
         }
 
         [Fact]
+        [Trait("Categories", "Fibo")]
         public void Fibonacci_MustNotHave4()
         {
             Assert.DoesNotContain(4, Collections.Fibonacci);
         }
 
         [Fact]
+        [Trait("Categories", "Fibo")]
         public void Fibonacci_ExpectedCollection()
         {
             List<int> expectedCollection = new List<int> { 1, 1, 2, 3, 5, 8, 13, 21 };
