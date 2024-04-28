4e3b12aeef18313cf7565147973fcb643b559195
Muhammad Ali <m.alikhan555@gmail.com>
2024-04-28 18:32:47 +0500

    Mocking in Unit Testing

---
+++ Mocking/Code/PersonalPhotos/Models/PhotoUploadViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace PersonalPhotos.Models;
+
+public class PhotoUploadViewModel
+{
+    [Required(ErrorMessage = "Please enter a description.")]
+    public string Description { get; set; }
+
+    [Required(ErrorMessage = "Please choose a photo!")]
+    public FormFile File { get; set; }
+}
